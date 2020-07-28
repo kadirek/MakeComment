25f9614d30f7b4f44dab0654ad66e85d0031969d
kadirek <kadirekbm@gmail.com>
2020-07-28 16:24:35 +0300

    soft input keyboard done!

--- app/src/main/java/com/example/makecomment/Activities/TvDetails.java
+++ app/src/main/java/com/example/makecomment/Activities/TvDetails.java
@@ -1,19 +1,25 @@
 package com.example.makecomment.Activities;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,6 +30,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -40,14 +49,15 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class TvDetails extends AppCompatActivity {
+public class TvDetails extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MyActivity";
 
     //UI
-    private TextView nameOfShow,userName;
+    private TextView nameOfShow;
     private ImageView userImg,imgOfShow;
-    private EditText commentField;
+    private TextView commentField;
     private Button sendComment;
+    private LinearLayout linearLayout;
 
     String channelNumber;
 
@@ -60,40 +70,46 @@ public class TvDetails extends AppCompatActivity {
     List<Comment> listOfComment;
     static String COMMENT_KEY = "CommentKey" ;
 
+    private BottomSheetDialog dialog;
+    static String titleForTry ;
+    private Toolbar toolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_details);
 
         //For full screen and hide actionbar
-        Window window = getWindow();
+       /* Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        getSupportActionBar().hide();
+        getSupportActionBar().hide();*/
 
-        nameOfShow = findViewById(R.id.titleOfShow);
-        userName = findViewById(R.id.userName);
+        //nameOfShow = findViewById(R.id.titleOfShow);
         userImg = findViewById(R.id.commentUserImg);
         imgOfShow = findViewById(R.id.imgOfShow);
         commentField = findViewById(R.id.commentField);
-        sendComment = findViewById(R.id.commentSendButton);
+        //sendComment = findViewById(R.id.commentSendButton);
         commentRV = findViewById(R.id.commentRV);
 
         mAuth = FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
         mDb = FirebaseDatabase.getInstance();
+
         if(mUser==null){
-            userImg.setVisibility(View.GONE);
+            //userImg.setVisibility(View.GONE);
             commentField.setVisibility(View.GONE);
-            sendComment.setVisibility(View.GONE);
+            //sendComment.setVisibility(View.GONE);
         }
         channelNumber = getIntent().getExtras().getString("whichChannel");
         //GET DATA from ParseAdapter class
         String imagePicasso = getIntent().getExtras().getString("imageUrl");//show's image
         final String title = getIntent().getExtras().getString("titleName");//show's title
+        titleForTry = title;
         final String time = getIntent().getExtras().getString("starttime");//show's start time
         String duration = getIntent().getExtras().getString("durationMinute");//show's duration (minute)
 
-        sendComment.setOnClickListener(new View.OnClickListener() {
+     /*   sendComment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sendComment.setVisibility(View.INVISIBLE);
@@ -121,19 +137,23 @@ public class TvDetails extends AppCompatActivity {
                     }
                 });
             }
-        });
+        });*/
+
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(titleForTry);
+        collapsingToolbar.setCollapsedTitleTypeface(ResourcesCompat.getFont(this,R.font.baloo));
+
+        commentField.setOnClickListener(this);
 
 
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
 
 
         Picasso.get().load(imagePicasso).into(imgOfShow);//head photo of tv program
-        nameOfShow.setText(title);
+        //nameOfShow.setText(title);
 
         if(signInAccount != null){
             Picasso.get().load(signInAccount.getPhotoUrl()).into(userImg);
-            userName.setVisibility(View.VISIBLE);
-            userName.setText("@"+signInAccount.getDisplayName());
         }
 
         SimpleDateFormat df = new SimpleDateFormat("HH:mm");//adding duration to start time and finding the finish hour
@@ -153,7 +173,7 @@ public class TvDetails extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 if (snapshot.hasChild(channelNumber)) {
-                    Toast.makeText(TvDetails.this, "Burda yorum var", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(TvDetails.this, "Burda yorum var", Toast.LENGTH_SHORT).show();///todo:önce yorum var mı diye kontrol ediyo
                     try {//Check if tv show finished or still live
                         Calendar now = Calendar.getInstance();
                         String current_hour = now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE);
@@ -184,7 +204,7 @@ public class TvDetails extends AppCompatActivity {
                                 @Override
                                 public void onDataChange(DataSnapshot dataSnapshot) {
                                     if(dataSnapshot.exists()){
-                                        Toast.makeText(TvDetails.this, "selam", Toast.LENGTH_SHORT).show();//yorum var ancak program hala bitmedi
+                                        //Toast.makeText(TvDetails.this, "selam", Toast.LENGTH_SHORT).show();todo:yorum var ancak program hala bitmedi
                                         Log.d(TAG, "kackadir "+ dataSnapshot.getRef());
                                         //bus number exists in Database
                                     } else {
@@ -213,6 +233,7 @@ public class TvDetails extends AppCompatActivity {
 
         initCommentRV();
     }
+
 
     private void initCommentRV() {
 
@@ -245,5 +266,92 @@ public class TvDetails extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.commentField){
 
+            showCommentDialog();
+        }
+    }
+
+    private void showCommentDialog() {
+
+        dialog = new BottomSheetDialog(this);
+        View commentView = LayoutInflater.from(this).inflate(R.layout.comment_dialog_layout,null);
+        final EditText commentText = (EditText) commentView.findViewById(R.id.dialog_comment_et);
+        final Button commentField = (Button) commentView.findViewById(R.id.dialog_comment_bt);
+        dialog.setContentView(commentView);
+
+        View parent = (View) commentView.getParent();
+        BottomSheetBehavior behavior = BottomSheetBehavior.from(parent);
+        commentView.measure(0,0);
+        behavior.setPeekHeight(commentView.getMeasuredHeight());
+
+        commentField.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String commentContent = commentText.getText().toString().trim();
+                if(!TextUtils.isEmpty(commentContent)){
+
+                    //commentOnWork(commentContent);
+                    dialog.dismiss();
+                   /* CommentDetailBean detailBean = new CommentDetailBean("Xiao Ming\n", commentContent,"just");
+                    adapter.addTheCommentData(detailBean);*/
+
+                    //sendComment.setVisibility(View.INVISIBLE);
+
+                    DatabaseReference dRef = mDb.getReference("Comment").child(channelNumber).push();
+                    String contentOfComment = commentContent;
+                    String uid = mUser.getUid();
+                    String uImg = mUser.getPhotoUrl().toString();
+                    String uName = mUser.getDisplayName();
+                    String showName = titleForTry;
+
+                    Comment comment = new Comment(contentOfComment, uid,uImg,uName,showName);
+
+                    dRef.setValue(comment).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(TvDetails.this, "Yorum eklendi", Toast.LENGTH_SHORT).show();
+                            commentField.setText("");
+                            //sendComment.setVisibility(View.VISIBLE);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(TvDetails.this, "Hata oluştu", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+                    Toast.makeText(TvDetails.this,"Yorum başarılı\n",Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(TvDetails.this,"Yorum içeriği boş olamaz\n",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        commentText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!TextUtils.isEmpty(charSequence) && charSequence.length()>2){
+                    commentField.setBackgroundColor(Color.parseColor("#F47676"));
+                }else {
+                    commentField.setBackgroundColor(Color.parseColor("#D8D8D8"));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        dialog.show();
+    }
 }
